f9d015b9d7f4a124c20f3b1b182246ae46518f60
mauricio <tu@email.com>
2024-04-03 11:26:37 -0500

    Creaccion de la funcion eliminar recurcion izquierda

--- main.py
+++ main.py
@@ -1,97 +1,121 @@
 from GestorArbolDerivacion import Gestor_Arbol_Derivacion
 
-
 class Grammar:
     def __init__(self, productions):
         self.productions = productions
         self.non_terminals = set(productions.keys())
 
     def eliminate_left_recursion(self):
-        for A in self.non_terminals:
-            for i in range(len(self.productions[A])):
-                if self.productions[A][i] and self.productions[A][i][0] == A:  # Agregar verificación
-                    alpha = self.productions[A][i][1:]
-                    self.productions[A].pop(i)
-                    new_non_terminal = A + "'"
-                    self.non_terminals.add(new_non_terminal)
-                    for production in self.productions:
-                        if production == A:
-                            continue
-                        self.productions[production].extend(
-                            [p + (new_non_terminal,) for p in self.productions[A]]
-                        )
-                    self.productions[new_non_terminal] = [alpha + (new_non_terminal,), ('',)]
+        new_grammar = {}
+        non_terminals = list(self.productions.keys())
 
+        for A in non_terminals:
+            productions_A = self.productions[A]
+            new_A = A + "2"
+            alpha_productions = []
+            beta_productions = []
 
-def creacion_de_diccionarios(lista, matriz):
-    diccionario = {}
-    for clave, datos in zip(lista, matriz):
-        diccionario[clave] = datos
-    return diccionario
+            for production in productions_A:
+                if production.startswith(A):
+                    alpha_productions.append(production[len(A):])
+                else:
+                    beta_productions.append(production)
 
-def eliminar_recursion(diccionario):
-    visitado = set()
+            if alpha_productions:
+                new_grammar[A] = [beta + new_A for beta in beta_productions]
+                new_grammar[new_A] = [alpha + new_A for alpha in alpha_productions] + [' ']
+            else:
+                new_grammar[A] = productions_A
 
-    def dfs(clave, camino):
-        if clave in camino:
-            diccionario[clave] = [valor for valor in diccionario[clave] if valor != clave]
-            return
+        return new_grammar
 
-        if clave in visitado:
-            return
+def convert_to_string_lists(grammar_dict):
+    converted_dict = {}
+    for key, value in grammar_dict.items():
+        converted_key = ''.join(key)
+        converted_value = [''.join(item) for item in value]
+        converted_dict[converted_key] = converted_value
+    return converted_dict
 
-        visitado.add(clave)
+def convert_to_tuple_lists(productions_dict, terminals):
+    converted_dict = {}
+    
+    # Función para particionar la cadena en no terminales y terminales
+    def partition_string(string, terminals, non_terminals):
+        partitions = []
+        current_partition = ""
+        i = 0
+        while i < len(string):
+            found = False
+            # Buscar las claves primero con las claves con más caracteres
+            for non_terminal in sorted(non_terminals, key=len, reverse=True):
+                if string[i:].startswith(non_terminal):
+                    partitions.append(current_partition)
+                    current_partition = ""
+                    partitions.append(non_terminal)
+                    i += len(non_terminal)
+                    found = True
+                    break
+            if not found:
+                # Particionar en terminales si no se encuentra un no terminal
+                for terminal in terminals:
+                    if string[i:].startswith(terminal):
+                        partitions.append(current_partition)
+                        current_partition = ""
+                        partitions.append(terminal)
+                        i += len(terminal)
+                        found = True
+                        break
+            if not found:
+                current_partition += string[i]
+                i += 1
+        partitions.append(current_partition)
+        return partitions
+    
+    # Obtener la lista de no terminales
+    non_terminals = list(productions_dict.keys())
+    
+    # Convertir cada producción a tuplas de caracteres particionando la cadena
+    for key, value in productions_dict.items():
+        converted_value = []
+        for item in value:
+            tuple_item = tuple(partition_string(item, terminals, non_terminals))
+            # Eliminar espacios extra
+            tuple_item = tuple(filter(lambda x: x != '', tuple_item))
+            converted_value.append(tuple_item)
+        converted_dict[key] = converted_value
+    return converted_dict
 
-        for valores in diccionario.get(clave, []):
-            for valor in valores:
-                dfs(valor, camino + [clave])
+def get_terminals(grammar):
+    terminals = set()
 
-    def dfs_indirecta(clave, camino, diccionario):
-        if clave in camino:
-            diccionario[camino[-1]] = [v for v in diccionario[camino[-1]] if v != clave]
-            return
+    for productions in grammar.values():
+        for production in productions:
+            for symbol in production:
+                if symbol.islower() or symbol.isdigit() or symbol == "'":
+                    terminals.add(symbol)
 
-        camino.append(clave)
-
-        for valor in diccionario.get(clave, []):
-            dfs_indirecta(valor, camino, diccionario)
-
-        camino.pop()
-
-    for clave in diccionario:
-        dfs(clave, [])
-        visitado.clear()
-        dfs_indirecta(clave, [], diccionario)
-
-    return diccionario
-
-
+    return list(terminals)
+def get_first_key(dictionary):
+    first_key = next(iter(dictionary.keys()), None)
+    return first_key
 def main():
-    lista_claves = ['S', 'A', 'B','C','D']
-    matriz_datos = [
-    [('A', 'B'),('S')],
-    [('a', 'A'),('B'),('A'),('C'),('')],
-    [('b', 'B'),('A'),('B'),('')],
-    [('c', 'C'),('D'),('A'),('')],
-    [('d', 'D'),('A'),('C'),('')]
-    ]   
-
-    diccionario_resultante = creacion_de_diccionarios(lista_claves, matriz_datos)
-
-    # Llamar a la función para eliminar recursión en el diccionario
-    productions_corregido = eliminar_recursion(diccionario_resultante)
-
-    grammar = Grammar(productions_corregido)
-    grammar.eliminate_left_recursion()
-
-
-    word = input("Ingrese una palabra para verificar si está en la gramática: ")
-    inicio_gramatica = next(iter(grammar.productions))[0]
-
+    ejemplo={
+        "AB": [("AB", "α"), ("AB", "β"), ("γ")],
+        "BC": [("BC", "α"), ("BC", "β"), ("δ")],
+        "CH": [("CH", "α"), ("CH", "δ"), ("1")],
+    }
+    terminales = get_terminals(ejemplo)
+    grammar = Grammar(convert_to_string_lists(ejemplo))
+    new_grammar = grammar.eliminate_left_recursion()
+    new_grammar = convert_to_tuple_lists(new_grammar,terminales)
+    print(new_grammar)
+    inicio_gramatica = get_first_key(new_grammar)
+    word = "γαβ"
     # Creacion del arbol de derivacion
     arbol_derivacion = Gestor_Arbol_Derivacion()
     #Devuelve el arbol ternario
-    arbol_ternario = arbol_derivacion.create_tree(inicio_gramatica,productions_corregido,word)
+    arbol_ternario = arbol_derivacion.create_tree(inicio_gramatica,new_grammar,word)
 
 
 if __name__ == "__main__":
